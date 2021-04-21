8ce7861dbb92e3b3759584aa5c5065fcef477d76
Comrade-Ivan <1335308@gmail.com>
2021-04-21 10:19:25 +0300

    cash

--- script.js
+++ script.js
@@ -101,18 +101,23 @@ function takeChange() {
 function createCoin(nominal) {
 
   let coinSrc = "";
+  let value = 0;
   switch (nominal) {
     case "10":
       coinSrc = "./img/10rub.png";
+      value = 10;
       break;
     case "5":
       coinSrc = "./img/5rub.png";
+      value = 5;
       break;
     case "2":
       coinSrc = "./img/2rub.png";
+      value = 2;
       break;
     case "1":
       coinSrc = "./img/1rub.png";
+      value = 1;
       break;
     default:
       console.error("Такой монеты не существует");
@@ -123,6 +128,7 @@ function createCoin(nominal) {
 
   const coin = document.createElement("img");
   coin.setAttribute("src", coinSrc);
+  coin.setAttribute("value", value);
   coin.className = "coin";
 
   coin.style.top = Math.floor(Math.random() * (changeContainerCoords.height - 52)) + "px";
@@ -133,6 +139,7 @@ function createCoin(nominal) {
   coin.onclick = function() {
     this.remove(); //Удаляет элемент
   }
+  coin.onmousedown = dragBill;
 
   //Анимируем падение монеты
   // setTimeout(() => {
@@ -142,5 +149,47 @@ function createCoin(nominal) {
   requestAnimationFrame(() => { //срабатывает перед рэндером
     coin.style.opacity = "100%";
     coin.style.transform = "translateY(0)";
-  })  
+  })
+}
+
+//Drag'n'Drop
+const bills = document.querySelectorAll(".wallet img");
+for (const bill of bills) {
+  bill.onmousedown = dragBill;
+}
+
+function dragBill(event) {
+  event.preventDefault();
+
+  const bill = this;
+  document.body.append(bill);
+  
+  const billCoords = bill.getBoundingClientRect();
+  bill.style.position = "absolute";
+  bill.style.transform = "rotate(90deg)";
+  
+  bill.style.top = event.clientY - billCoords.height/2 + "px";
+  bill.style.left = event.clientX - billCoords.width/2 + "px";
+
+  function onMouseMove(event) {
+    bill.style.top = event.clientY - billCoords.height/2 + "px";
+    bill.style.left = event.clientX - billCoords.width/2 + "px";
+  }
+
+  document.addEventListener("mousemove", onMouseMove);
+
+  bill.onmouseup = function(event) {
+    document.removeEventListener("mousemove", onMouseMove);
+    bill.onmouseup = null;
+
+    bill.hidden = true;
+    const elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    bill.hidden = false;
+    bill.style.transform = "rotate(0deg)";
+    if ( elemBelow.classList.contains("bill-acc") ) {
+      let value = +bill.getAttribute("value");
+      balance.value = +balance.value + value;
+      bill.remove();
+    }
+  }
 }
